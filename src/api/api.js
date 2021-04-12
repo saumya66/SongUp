1203e57f3d95b406b56ac98fce0a8da0980499f8
Saumya <saumyanayak264@gmail.com>
2021-04-12 16:11:02 +0530

    Fixed major bugs

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://song-up.herokuapp.com" });
 
 API.interceptors.request.use((req) => {
 	if (localStorage.getItem("profile")) {
